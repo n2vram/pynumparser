5a584bfd5a7287ccc59385a2f2d87175c461f7fa
NVRAM <nvram@users.sf.net>
2016-01-03 00:23:07 -0700

    Version 1.0.1

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'pynumparser',
   packages = ['pynumparser'],
-  version = '1.0',
+  version = '1.0.1',
   description = 'A library for argument parsing numbers and number sequences',
   author = 'NVRAM',
   author_email = 'nvram@users.sourceforge.net',
   url = 'https://github.com/n2vram/pynumparser', # use the URL to the github repo
-  download_url = 'https://github.com/n2vram/pynumparser/tarball/1.0',
+  download_url = 'https://github.com/n2vram/pynumparser/tarball/1.0.1',
   keywords = ['argparse', 'numparser', 'parser', 'ArgumentParser', 'command-line'], # arbitrary keywords
   classifiers = [],
 )
